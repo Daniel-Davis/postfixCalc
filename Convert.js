c754490ee8bf4f5337ac25f3a9368695146436e9
Daniel-Davis <ddv9461@saintjoe.edu>
2015-01-30 12:35:56 -0600

    Update Convert.js

--- Convert.js
+++ Convert.js
@@ -5,23 +5,40 @@ function convert(Input) {
   var operands = new Stack();
   var operators = new Stack();
   
-  while (i = 0; i <= Stack.length(); i++) {
-    if ( Input == 1 || Input == 2 || Input == 3 || Input == 4 || Input == 5 || Input == 6 || Input == 7 || Input == 8 || Input == 9 || Input == 0) {
-      operands.push(Input);
-      console.log('Input has been pushed to operands stack');
+  while (i = 0; i <= Stack.length(); i++) { // goes through stack length
+  
+    if ( Input == 1 || Input == 2 || Input == 3 || Input == 4 || Input == 5 || Input == 6 || Input == 7 || Input == 8 || Input == 9 || Input == 0) { // if a number
+      operands.push(Input); // pushes input into the operands stack
+      console.log('Input has been pushed to operands stack'); // lets me know job is done
     }// end of if
-    else if(Input == '' || Input == "" || Input == '.' || Input == '(' || Input == ')') {
-      Stack.splice(Input);
-      console.log('Input has be spliced');
+    
+    else if (Input == '' || Input == "" || Input == '.') { // if a spacial character 
+      Stack.splice(Input); // delete that character
+      console.log('Input has be spliced'); // let me know job is done
     }// end of else if
-    else if (Input == '+' || Input == '-' || Input == '*' || Input == '/') {
-      operators.push(Input);
-      console.log('Input has been pushed to operators');
+    
+    else if (Input == '+' || Input == '-' || Input == '*' || Input == '/') { // if character is an operator
+      operators.push(Input); // push it onto operators stack
+      console.log('Input has been pushed to operators'); // job is done
     }// end of else if
-    else {
-      console.log('Error, Invalid Character');
+    
+    else if (Input == '(') { // if the beginning of a private calculation
+      convert(); // start up converting that section
+      console.log('Starting private calc'); // job is going
+    }// end of else if
+    
+    else if(Input == ')') { // if the end of a private calc
+      continue; // break from recursion and continue conversion
+      console.log('end of private clac'); // job is done
+    }// end of else if
+    
+    else { // anything else
+      console.log('Error, Invalid Character'); // why would you put that in here
     }// end of else
+    
   }// end of while loop
   
   operands.foreach(console.log(element));
   operators.foreach(console.log(element));
+  
+}// end of function
