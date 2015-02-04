c3f069b2396fedd34b116ca386770e8333b13364
Daniel-Davis <ddv9461@saintjoe.edu>
2015-02-03 21:31:22 -0800

    Update Calc.js

--- Calc.js
+++ Calc.js
@@ -1,6 +1,11 @@
 // Daniel Davis, postfix calc, calculates postfix notation//
 
-function calc(Stack1, Stack2) {
+function calc(Stack) {
   
-  while (var i=0, i<=Stack1.length, i++) {
-    if (Stack1[i] 
+  var x=0;
+  var y=0;
+  
+  while (var i=0, i<=Stack.length, i++) {
+    if (Stack[i] == 1 || Stack[i] == 2 || Stack[i] == 3 || Stack[i] == 4 || Stack[i] == 5 || Stack[i] == 6 || Stack[i] == 7 || Stack[i] == 8 || Stack[i] == 9 || Stack[i] == 0) {
+        
+    }// end of if
